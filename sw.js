2b0b0f34a0238ebfd4393567b509ecb348f528e0
LeeDiggle <thedigglefamily@virginmedia.com>
2026-09-08 09:18:01 +0100

    Update service worker cache version and files

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "fortnight-tracker-v3";
+const CACHE_NAME = "fortnight-tracker-v4";
 
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=3",
-  "./app.js?v=3",
+  "./styles.css?v=4",
+  "./app.js?v=4",
   "./manifest.webmanifest",
   "./icon-192.png",
   "./icon-512.png"
@@ -14,44 +14,67 @@ self.addEventListener("install", event => {
   self.skipWaiting();
 
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(APP_FILES))
+    caches
+      .open(CACHE_NAME)
+      .then(cache =>
+        cache.addAll(APP_FILES)
+      )
   );
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys()
+    caches
+      .keys()
       .then(keys =>
         Promise.all(
           keys
-            .filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
+            .filter(
+              key =>
+                key !== CACHE_NAME
+            )
+            .map(
+              key =>
+                caches.delete(key)
+            )
         )
       )
-      .then(() => self.clients.claim())
+      .then(() =>
+        self.clients.claim()
+      )
   );
 });
 
 self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") {
+  if (
+    event.request.method !== "GET"
+  ) {
     return;
   }
 
   event.respondWith(
-    fetch(event.request)
+    fetch(
+      event.request
+    )
       .then(response => {
-        const copy = response.clone();
+        const copy =
+          response.clone();
 
-        caches.open(CACHE_NAME)
+        caches
+          .open(CACHE_NAME)
           .then(cache => {
-            cache.put(event.request, copy);
+            cache.put(
+              event.request,
+              copy
+            );
           });
 
         return response;
       })
       .catch(() =>
-        caches.match(event.request)
+        caches.match(
+          event.request
+        )
       )
   );
 });
